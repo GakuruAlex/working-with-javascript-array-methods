b3dd7ff9e1e044c4e405f46f0beb6b532c81024c
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-08-07 10:55:51 +0300

    Initial commit

---
+++ arrayMethods.js
@@ -0,0 +1,46 @@
+const superHeroes = [
+"Superman",
+"Supergirl",
+"Arrow",
+"Flash",
+
+];
+
+/* Using the .push() method  to add  element to end of  array*/
+
+function addElementsWithPush(customArray,elementToAdd){
+
+    console.log(`Array before using push() method   ${customArray}`);
+
+    console.log(`The size of array before push method is : ${customArray.length}`)
+
+    console.log(`Push method returns size of new array : in this case : ${customArray.push(elementToAdd)}`);
+
+    console.log(`Array after array.push() ${customArray}`);
+
+}
+
+/*using the unshift operator to add element to start of array
+destructive */
+
+function addElementsWithUnshift(customArray,elementToAdd){
+
+    console.log(`Array before using unshift method: ${customArray}` );
+
+    console.log(`performing "console.log(array.unshift(someElement))" yields : ${customArray.unshift(elementToAdd)}`);
+
+    console.log(`Array after unshift: ${customArray}`);
+
+
+
+}
+
+/*using the spread operator to add element to start of array
+non destructive */
+const newSuperheroesSpreadOperator=["CatWoman", ...superHeroes];
+
+addElementsWithUnshift(superHeroes,"Doctor Strange");
+
+console.log(`New superheroes array using spread operator ["CatWoman", ...superHeroes] is ${newSuperheroesSpreadOperator} `);
+
+/* Using the  */
